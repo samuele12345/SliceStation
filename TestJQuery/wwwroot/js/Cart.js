83470c5158a54bd9b1636b8b15f4b094dfb9fdde
Samuele <samueleriva2016@gmail.com>
2026-07-04 15:58:55 +0200

    aggiunti dettagli a cart

--- TestJQuery/wwwroot/js/Cart.js
+++ TestJQuery/wwwroot/js/Cart.js
@@ -1,5 +1,9 @@
-﻿@*
-    For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-*@
-@{
-}
+﻿$(document).ready(
+    $.ajax({
+        url: "/Pizza/Cart",
+        type: "GET",
+        success: function (OrdPizzas) {
+
+        }
+    })
+)
