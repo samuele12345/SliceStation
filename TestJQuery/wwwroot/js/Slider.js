5e3b7f82d4c34f65bd3e85efe9a5ff7ca57f5a48
Samuele <samueleriva2016@gmail.com>
2026-07-05 23:14:16 +0200

    js slider inizio

--- TestJQuery/wwwroot/js/Slider.js
+++ TestJQuery/wwwroot/js/Slider.js
@@ -1,6 +1,4 @@
-﻿namespace TestJQuery.wwwroot.js
-{
-    public class Slider
-    {
-    }
-}
+﻿const slider = document.querySelector(".slider");
+const slides = document.querySelector(".slides");
+const slide = document.querySeòlectorAll(".slide");
+
