90b9b7038de0b4151ef29c287866b3cb17c6ffe9
Samuele <samueleriva2016@gmail.com>
2026-07-06 17:43:28 +0200

    completato slider index, aggiungere poi qualche dettaglio

--- TestJQuery/wwwroot/js/Slider.js
+++ TestJQuery/wwwroot/js/Slider.js
@@ -1,4 +1,42 @@
 ﻿const slider = document.querySelector(".slider");
 const slides = document.querySelector(".slides");
-const slide = document.querySeòlectorAll(".slide");
+const slide = document.querySelectorAll(".slide");
 
+
+
+if (slider) {
+    let slideIndex = 0;
+    let intervalId = null;
+
+    window.addEventListener("DOMContentLoaded", slideDefault)
+
+
+    function slideDefault() {
+        slide[slideIndex].classList.add("active");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+
+    function currentSlide(index) {
+        if (index == slide.length) {
+            index = 0;
+        } else if (index == -1) {
+            index = slide.length - 1;
+        }
+
+        slide.forEach(slide => {
+            slide.classList.remove("active");
+        })
+
+        slideIndex = index;
+        slide[slideIndex].classList.add("active");
+    }
+
+
+    function nextSlide() {
+        slideIndex++;
+        clearInterval(intervalId);
+        intervalId = setInterval(nextSlide, 5000);
+        currentSlide(slideIndex);
+    }
+
+}
