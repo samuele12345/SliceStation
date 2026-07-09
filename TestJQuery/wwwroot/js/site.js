f64dca8d77ad48e5ceedaeef3d8b17d5755eb0f2
Samuele <samueleriva2016@gmail.com>
2026-07-09 14:32:55 +0200

    miglioratom layout e estetica della pagina menu, continuare privacy (iniziata) e valutare di aggiungere un footer minimale e i cookie

--- TestJQuery/wwwroot/js/site.js
+++ TestJQuery/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const xBut = document.querySelector(".esc");
+const pop = document.querySelector(".pop-up");
 
-// Write your JavaScript code.
+xBut.addEventListener("click", () => {
+    pop.classList.add("hidden");
+})
